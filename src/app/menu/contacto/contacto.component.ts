33397ea82a46762c9ecf3989c2c5426382d4693d
Julian Narvaez <julian_narvaezna@fet.edu.co>
2024-03-14 21:11:56 -0500

    Create contacto.component.ts

---
+++ src/app/menu/contacto/contacto.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto',
+  standalone: true,
+  imports: [],
+  templateUrl: './contacto.component.html',
+  styleUrl: './contacto.component.css'
+})
+export class ContactoComponent {
+  mensaje(){
+    alert("Grcias por su Comentario")
+  }
+}
