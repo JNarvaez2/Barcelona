21d4b0f2af9adf73c8896255266112bc03f0cdfa
Julian Narvaez <julian_narvaezna@fet.edu.co>
2024-03-14 21:10:17 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioComponent } from './menu/inicio/inicio.component';
+import { ServicioComponent } from './menu/servicio/servicio.component';
+import { ContactoComponent } from './menu/contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    InicioComponent,
+    ServicioComponent,
+    ContactoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
