8fb80f980c7473e824e79e8762bdb78516345023
Julian Narvaez <julian_narvaezna@fet.edu.co>
2024-03-14 21:10:23 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './menu/inicio/inicio.component';
+import { ServicioComponent } from './menu/servicio/servicio.component';
+import { ContactoComponent } from './menu/contacto/contacto.component';
+import { LoginComponent } from './menu/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '',component: InicioComponent},
+    { path: 'inicio', component: InicioComponent },
+    { path: 'servicio', component: ServicioComponent },
+    { path: 'contacto', component: ContactoComponent },
+    {path:'login',component:LoginComponent}
+    
+];
